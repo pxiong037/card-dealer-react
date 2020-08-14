4a70a1580b44cb115e8d435193f5a500a2dce9c8
Prechar <pxiong037@gmail.com>
2020-08-14 05:30:28 +0000

    Updates

---
+++ precache-manifest.9c076a0f707063c1958dba9b768fd691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a97ee1cf3ec90e45416db81a441ff1",
+    "url": "/card-dealer-react/index.html"
+  },
+  {
+    "revision": "1e01916d559d0bd9a51f",
+    "url": "/card-dealer-react/static/css/main.6d93fbea.chunk.css"
+  },
+  {
+    "revision": "16559fc4d8d77357ff64",
+    "url": "/card-dealer-react/static/js/2.4bfb321a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-dealer-react/static/js/2.4bfb321a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e01916d559d0bd9a51f",
+    "url": "/card-dealer-react/static/js/main.fff8082a.chunk.js"
+  },
+  {
+    "revision": "aa5d0b3cc45eb3a30751",
+    "url": "/card-dealer-react/static/js/runtime-main.dcf5dc36.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/card-dealer-react/precache-manifest.9c076a0f707063c1958dba9b768fd691.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/card-dealer-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
